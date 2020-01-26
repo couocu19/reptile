6731e62fe0de0fbad54f1a43ada650306114a41b
coucou <1131088589@qq.com>
2020-01-26 19:23:48 +0800

    1.26

--- src/main/java/reptile/ConnectJWGL.java
+++ src/main/java/reptile/ConnectJWGL.java
@@ -2,10 +2,10 @@ package reptile;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.http.cookie.Cookie;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -33,7 +33,7 @@ public class ConnectJWGL {
         getCsrftoken();
         getRSApublickey();
     }
-
+    ///jwglxt/xtgl/login_slogin.html?language=zh_CN&_t=1579964735360
     // 获取csrftoken和Cookies
     private void getCsrftoken(){
         try{
@@ -41,6 +41,10 @@ public class ConnectJWGL {
             connection.header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0");
             response = connection.execute();
             cookies = response.cookies();
+            for(Object o:cookies.keySet()){
+                System.out.println(cookies.get(o));
+
+            }
             //保存csrftoken
             document = Jsoup.parse(response.body());
             csrftoken = document.getElementById("csrftoken").val();
@@ -49,6 +53,7 @@ public class ConnectJWGL {
         }
     }
 
+    ///jwglxt/cjcx/cjcx_cxDgXscj.html?gnmkdm=N305005&layout=default&su=04182098
     // 获取公钥并加密密码
     public void getRSApublickey() throws Exception{
         connection = Jsoup.connect(url+ "/jwglxt/xtgl/login_getPublicKey.html?" +
@@ -77,6 +82,7 @@ public class ConnectJWGL {
                 .method(Connection.Method.POST).execute();
 
         response = connection.execute();
+        //System.out.println(response.body());
         document = Jsoup.parse(response.body());
         if(document.getElementById("tips") == null){
             System.out.println("欢迎登陆");
@@ -132,37 +138,64 @@ public class ConnectJWGL {
         }
     }
 
+    //jwglxt/cjcx/cjcx_cxDgXscj.html?gnmkdm=N305005&layout=default&su=04182098
     // 获取成绩信息
     public void getStudentGrade(int year , int term) throws Exception {
+        Connection.Response response1 = null;
+        Map<String,String> cookies1 = new HashMap<>();
         Map<String,String> datas = new HashMap<>();
         datas.put("xnm",String.valueOf(year));
         datas.put("xqm",String.valueOf(term * term * 3));
         datas.put("_search","false");
         datas.put("nd",String.valueOf(new Date().getTime()));
-        datas.put("queryModel.showCount","20");
+        datas.put("queryModel.showCount","15");
         datas.put("queryModel.currentPage","1");
         datas.put("queryModel.sortName","");
         datas.put("queryModel.sortOrder","asc");
-        datas.put("queryModel.sortName","");
-        datas.put("time","0");
+        datas.put("time","1");
+        //http://www.zfjw.xupt.edu.cn/jwglxt/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdm=N305005
+//        connection = Jsoup.connect(url+"/jwglxt/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdm=N305005"+stuNum);
+//        connection = Jsoup.connect(url+ "/jwglxt/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdm=N305005");
+//        connection.header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0");
+//        response = connection.cookies(cookies).method(Connection.Method.POST)
+//                .data(datas).ignoreContentType(true).execute();
+        //connection = Jsoup.connect(url+ "/jwglxt/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdm=N305005&layout=default&su=04182098");
+        connection.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36")
+                  .timeout(10000).ignoreContentType(true).execute();
 
-        connection = Jsoup.connect(url+ "/jwglxt/cjcx/cjcx_cxDgXscj.html?gnmkdm=N305005&layout=default&su=" + stuNum);
-        connection.header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0");
-        response = connection.cookies(cookies).method(Connection.Method.POST)
-                .data(datas).ignoreContentType(true).execute();
-        connection = Jsoup.connect(url+ "/jwglxt/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdm=N305005");
-        connection.header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0");
-        response = connection.cookies(cookies).method(Connection.Method.POST)
-                .data(datas).ignoreContentType(true).execute();
-        JSONObject jsonObject = JSON.parseObject(response.body());
-        JSONArray gradeTable = JSON.parseArray(jsonObject.getString("items"));
-        for (Iterator iterator = gradeTable.iterator(); iterator.hasNext();) {
-            JSONObject lesson = (JSONObject) iterator.next();
-            System.out.println(lesson.getString("kcmc") + " " +
-                    lesson.getString("jsxm") + " " +
-                    lesson.getString("bfzcj") + " " +
-                    lesson.getString("jd"));
+
+//        response1 = connection.cookies(cookies).method(Connection.Method.POST)
+//                .data(datas).ignoreContentType(true).execute();
+
+
+        response1 = Jsoup.connect(url+"/jwglxt/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdm=N305005")
+               // .header("Accept", "*/*")
+                //.header("Accept-Encoding", "gzip, deflate")
+                //.header("Accept-Language","zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3")
+                //.header("Content-Type", "application/json;charset=UTF-8")
+                .header("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36")
+                .data(datas).timeout(10000).ignoreContentType(true).execute();
+
+        cookies1 = response1.cookies();
+        String cookie = null;
+        for(Object c:cookies1.keySet()){
+            cookie = cookies1.get(c);
         }
+        System.out.println(cookie);
+        response1.header("Cookie","JSESSIONID="+cookie);
+        //  response1 = connection.method(Connection.Method.POST).execute();
+
+        System.out.println(response1.body());
+        System.out.println(1);
+//        JSONObject jsonObject = JSON.parseObject(response.body());
+//        JSONArray gradeTable = JSON.parseArray(jsonObject.getString("items"));
+//        for (Iterator iterator = gradeTable.iterator(); iterator.hasNext();) {
+//            JSONObject lesson = (JSONObject) iterator.next();
+//            System.out.println(lesson.getString("kcmc") + " " +
+//                    lesson.getString("jsxm") + " " +
+//                    lesson.getString("bfzcj") + " " +
+//                    lesson.getString("jd"));
+//        }
     }
 
     public void logout() throws Exception {
--- src/main/java/reptile/Main.java
+++ src/main/java/reptile/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
+    //
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         System.out.print("请输入学号:");
@@ -13,17 +14,17 @@ public class Main {
         ConnectJWGL connectJWGL = new ConnectJWGL(stuNum,password);
         connectJWGL.init();
         if(connectJWGL.beginLogin()){
-            connectJWGL.getStudentInformaction();
-            System.out.println("---查询课表---");
+           // connectJWGL.getStudentInformaction();
+           // System.out.println("---查询课表---");
             System.out.print("输入学年（2018-2019就输2018）:");
             int year = input.nextInt();
             System.out.print("输入学期（1或2）:");
             int term = input.nextInt();
-            connectJWGL.getStudentTimetable(year,term);
-            System.out.print("输入学年（2018-2019就输2018）:");
-            year = input.nextInt();
-            System.out.print("输入学期（1或2）:");
-            term = input.nextInt();
+            //connectJWGL.getStudentTimetable(year,term);
+            //System.out.print("输入学年（2018-2019就输2018）:");
+            //year = input.nextInt();
+            //System.out.print("输入学期（1或2）:");
+            //term = input.nextInt();
             connectJWGL.getStudentGrade(year,term);
         }
         connectJWGL.logout();
